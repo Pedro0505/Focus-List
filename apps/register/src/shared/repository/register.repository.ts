ee94e50efea10a37c98c68490ed74e6c594eb29f
pedro <pedrohrds05@hotmail.com>
2023-04-05 11:32:14 -0300

    feat: adding register repository

--- apps/register/src/shared/repository/register.repository.ts
+++ apps/register/src/shared/repository/register.repository.ts
@@ -3,12 +3,13 @@ import { InjectModel } from '@nestjs/mongoose';
 import { REGISTER } from 'src/constants/modelNames';
 import { Register } from 'src/db/register.model';
 import { Model } from 'mongoose';
+import User from '../entity/User';
 
 @Injectable()
 export class RegisterRepository {
   constructor(@InjectModel(REGISTER) private registerModel: Model<Register>) {}
 
-  public async addUser(user: { name: string; email: string }) {
+  public async insertUser(user: User) {
     return this.registerModel.create(user);
   }
 }
