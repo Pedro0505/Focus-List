768091878b06b49295240869faddc6953c64cf82
pedro <pedrohrds05@hotmail.com>
2023-04-05 11:33:15 -0300

    feat: adding module service

--- apps/register/src/shared/register.module.ts
+++ apps/register/src/shared/register.module.ts
@@ -1,11 +1,21 @@
 import { Module } from '@nestjs/common';
-import { registerForFeature, registerForRoot } from 'src/db/register.db';
+import { ConfigModule } from '@nestjs/config';
+import { QueueNames } from '../constants/queueNames';
+import { registerForFeature, registerForRoot } from '../db/register.db';
+import { RmqModule } from '../infra/rabbitmq/rmq.module';
 import { RegisterController } from './controller/register.controller';
 import { RegisterRepository } from './repository/register.repository';
 import { RegisterService } from './service/register.service';
 
 @Module({
-  imports: [registerForFeature, registerForRoot],
+  imports: [
+    registerForFeature,
+    registerForRoot,
+    RmqModule.register({
+      name: QueueNames.REGISTER,
+    }),
+    ConfigModule.forRoot({ isGlobal: true }),
+  ],
   controllers: [RegisterController],
   providers: [RegisterService, RegisterRepository],
 })
