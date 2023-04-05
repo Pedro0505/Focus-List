59c5efb765b0d9ffdb5868949e98c17463f0049f
pedro <pedrohrds05@hotmail.com>
2023-04-05 11:25:57 -0300

    feat(db): adding field password

--- apps/register/src/db/register.model.ts
+++ apps/register/src/db/register.model.ts
@@ -4,6 +4,7 @@ export const RegisterSchema = new mongoose.Schema(
   {
     name: { type: String, required: true },
     email: { type: String, required: true },
+    password: { type: String, required: true },
   },
   { versionKey: false },
 );
@@ -12,4 +13,5 @@ export interface Register {
   id: string;
   email: string;
   name: string;
+  password: string;
 }
