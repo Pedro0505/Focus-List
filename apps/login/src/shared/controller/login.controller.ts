f9160591f622f462fdab8d5acf3b3378ea7b1ef1
pedro <pedrohrds05@hotmail.com>
2023-05-09 16:52:04 -0300

    feat: adding a func to recive and print messages in create_user queue

--- apps/login/src/shared/controller/login.controller.ts
+++ apps/login/src/shared/controller/login.controller.ts
@@ -1,8 +1,18 @@
 import { Controller } from '@nestjs/common';
+import { Ctx, EventPattern, Payload, RmqContext } from '@nestjs/microservices';
+import { RmqService } from 'src/infra/rabbitmq/rmq.service';
 
 @Controller()
 export class LoginController {
+  constructor(private readonly rmqService: RmqService) {}
+
   public helloWorld() {
     return 'Hello World';
+  }
+
+  @EventPattern('create_user')
+  public login(@Payload() data: any, @Ctx() context: RmqContext) {
+    console.log(data);
+    this.rmqService.ack(context);
   }
 }
