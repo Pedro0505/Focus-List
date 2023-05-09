59c48a82e2fbeca578dfdf612b53c34425647e99
pedro <pedrohrds05@hotmail.com>
2023-05-09 17:03:21 -0300

    feat: encrypting the passwords

--- apps/register/src/shared/controller/register.controller.ts
+++ apps/register/src/shared/controller/register.controller.ts
@@ -1,13 +1,16 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import User from '../entity/User';
 import { RegisterService } from '../service/register.service';
+import IUser from '../interface/IUser';
 
 @Controller('/register')
 export class RegisterController {
   constructor(private registerService: RegisterService) {}
 
   @Post()
-  public async addUser(@Body() user: User) {
-    return await this.registerService.addUser(user);
+  public async addUser(@Body() user: IUser) {
+    const newUser = new User(user.name, user.password, user.password);
+
+    return await this.registerService.addUser(newUser);
   }
 }
